e4bdcc7ea02d026fcb2d7a5a5c540f1febc01ce6
Jay Watson <jaywatson1982@gmail.com>
2022-08-10 05:56:33 -0500

    Add tf files

---
+++ StorageAccount-Terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "dev2"
+    storage_account_name = "storemfwmw3heqnyuk"
+    container_name       = "testcontainer"
+    key                  = "terraform.state"
+  }
+}
+
+provider "azurerm" {
+  version = "2.0.0"
+  features {}
+}
+
+resource "azurerm_storage_account" "TechTargetSG" {
+  Name                     = "ttstorage92"
+  resource_group_name      = var.resourceGroupName
+  Location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  tags = {
+    environment = "development"
+  }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Play and Learn
+
+[How to deploy Terraform code in an Azure DevOps pipeline](https://www.techtarget.com/searchcloudcomputing/tip/How-to-deploy-Terraform-code-in-an-Azure-DevOps-pipeline)
